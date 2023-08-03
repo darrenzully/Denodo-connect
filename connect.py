93fb47d50644d7248cee092bb5de5f6632835b8a
Dario Renzulli <dario.renzulli@gmail.com>
2023-08-03 10:13:54 -0300

    connect improved

--- connect.py
+++ connect.py
@@ -1,36 +1,43 @@
+from socket import gethostname
+
 import jaydebeapi as dbdriver
 import jpype
 
-USERAGENT = "denodo-connect"
 
-def connect(hostname: str, port: str, vdbname: str, usr: str, pss: str, driver_path: str):
-    conn_uri = f"jdbc:vdb://{hostname}:{port}/{vdbname}?ssl=true&queryTimeout=3000&userAgent={USERAGENT}"
+# Constants for SSL options
+QUERY_TIMEOUT = 3000
+SSL_ENABLED = True
+
+def connect(hostname: str, port: str, vdbname: str, usr: str, pss: str, driver_path: str, user_agent: str = None):
+    if not user_agent:
+        client_hostname = gethostname()
+        user_agent = f"{dbdriver.__name__}-{client_hostname}"
+
+    conn_uri = f"jdbc:vdb://{hostname}:{port}/{vdbname}?ssl={SSL_ENABLED}&queryTimeout={QUERY_TIMEOUT}&userAgent={user_agent}"
     user = usr
     passwd = pss
- 
-    conn = dbdriver.connect( "com.denodo.vdp.jdbc.Driver",
-                            conn_uri,
-                            driver_args = {"user": user,
-                                            "password": passwd},
-                            jars = driver_path
-                            )
-    return conn
 
-def check_and_connect(hostname: str, port: str, vdbname: str, usr: str, pss: str, driver_path: str, trust_path: str):
-    
-    conn_uri = f"jdbc:vdb://{hostname}:{port}/{vdbname}?ssl=true&queryTimeout=3000&userAgent={USERAGENT}"
-   
+    return dbdriver.connect("com.denodo.vdp.jdbc.Driver",
+                            conn_uri,
+                            driver_args={"user": user,
+                                         "password": passwd},
+                            jars=driver_path)
+
+def check_and_connect(hostname: str, port: str, vdbname: str, usr: str, pss: str, driver_path: str, trust_path: str, user_agent: str = None):
+    if not user_agent:
+        client_hostname = gethostname()
+        user_agent = f"{dbdriver.__name__}-{client_hostname}"
+
+    conn_uri = f"jdbc:vdb://{hostname}:{port}/{vdbname}?ssl={SSL_ENABLED}&queryTimeout={QUERY_TIMEOUT}&userAgent={user_agent}"
+
     user = usr
     passwd = pss
- 
-    if (not jpype.isJVMStarted()):
-        jpype.startJVM(jpype.getDefaultJVMPath(), "-Djava.class.path=" + driver_path,
-            "-Djavax.net.ssl.trustStore=" + trust_path)
- 
-    conn = dbdriver.connect( "com.denodo.vdp.jdbc.Driver",
+
+    if not jpype.isJVMStarted():
+        jpype.startJVM(jpype.getDefaultJVMPath(), "-Djava.class.path=" + driver_path, "-Djavax.net.ssl.trustStore=" + trust_path)
+
+    return dbdriver.connect("com.denodo.vdp.jdbc.Driver",
                             conn_uri,
-                            driver_args = {"user": user,
-                                            "password": passwd},
-                            jars = driver_path
-                            )
-    return conn
+                            driver_args={"user": user,
+                                         "password": passwd},
+                            jars=driver_path)
